f7e7d3a23a3b3d2f2ae6d9a3058726a5f4cbc695
Megan Honey <mjthoney@gmail.com>
2022-10-08 21:58:55 +0000

    Finish getAllProperties refactoring with all search options

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -131,25 +131,32 @@ const getAllProperties = function(options, limit = 10) {
   JOIN property_reviews ON properties.id = property_id
   WHERE 1 = 1
   `;
-
+/* If statements for all options on search page */
   if (options.owner_id) {
     queryParams.push(options.owner_id);
     queryString += `AND properties.owner_id = $${queryParams.length} `;
   }
-
   if (options.city) {
     queryParams.push(`%${options.city}%`);
     queryString += `AND city LIKE $${queryParams.length}`;
   }
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(parseInt(options.minimum_price_per_night));
+    queryParams.push(parseInt(options.maximum_price_per_night));
+    queryString += `AND properties.cost_per_night > $${queryParams.length - 1} AND properties.cost_per_night < $${queryParams.length}`;
+  }
+  if (options.minimum_rating) {
+    queryParams.push(parseInt(options.minimum_rating));
+    queryString += `AND property_reviews.rating >= $${queryParams.length}`;
+  }
 
+  /* Add limit to parameters last */
   queryParams.push(limit);
   queryString += `
   GROUP BY properties.id
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
-
-  console.log(queryString, queryParams);
 
   return pool
   .query(queryString, queryParams)
