e755822b47360536ebfb372fbf14c01da6eee48c
Megan Honey <mjthoney@gmail.com>
2022-10-08 22:21:07 +0000

    Refactor addProperty to add property to database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -177,9 +177,23 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [];
+  const queryString = `
+    INSERT INTO properties (title, description, number_of_bedrooms, number_of_bathrooms, parking_spaces, cost_per_night, thumbnail_photo_url, cover_photo_url, street, country, city, province, post_code, owner_id)
+    VALUES ($1, $2, $3, $4, $5, $6*100, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *
+  `;
+  for (const val of Object.keys(property)) {
+    queryParams.push(property[val]);
+  }
+
+  return pool
+    .query(queryString,queryParams)
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.addProperty = addProperty;
